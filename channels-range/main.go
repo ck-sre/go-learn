6face8a055e3d6cff15c6869862460c2d8a66b1d
Cyril Kurian <cyrilkurian01@gmail.com>
2023-03-22 23:52:23 +0800

    channels blocking

--- channels-range/main.go
+++ channels-range/main.go
@@ -3,16 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	f := make(chan int)
-	go func() {
-		for i := 0; i < 5; i++ {
-			f <- i
-		}
-		close(f)
-	}()
+	t := make(chan int)
+	nt := make(chan int)
+	q := make(chan int)
 
-	for k := range f {
-		fmt.Println(k)
+	go sendThrees(t, nt, q)
+	getThrees(t, nt, q)
+	fmt.Println("above is all")
+}
+
+func sendThrees(tr, ntr, qr chan<- int) {
+	for i := 1; i < 100; i++ {
+		if i%3 == 0 {
+			tr <- i
+		} else {
+			ntr <- i
+		}
 	}
-	fmt.Println("done sending and receiving")
+	qr <- 0
+}
+
+func getThrees(tg, ntg, qg <-chan int) {
+	for {
+		select {
+		case i := <-tg:
+			fmt.Println("from the multiple of 3 ", i)
+		case i := <-ntg:
+			fmt.Println("from a non multiple of 3 ", i)
+		case i := <-qg:
+			fmt.Println("quit signal received ", i)
+			return
+		}
+	}
+
 }
