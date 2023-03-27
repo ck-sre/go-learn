05d6ade74ff9de5d5fb2da264083b39351253e20
Cyril Kurian <cyrilkurian01@gmail.com>
2023-03-27 21:21:28 +0800

    throttling example

--- channels-fan-in-examples/main.go
+++ channels-fan-in-examples/main.go
@@ -2,52 +2,52 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
+	"time"
 )
 
 func main() {
-	m3 := make(chan int)
-	nm3 := make(chan int)
-	p := make(chan int)
+	fo := make(chan int)
+	fi := make(chan int)
 
-	go spreadChans(m3, nm3)
-	go getToPipe(m3, nm3, p)
-	for k := range p {
-		fmt.Println(k)
+	go fillChan(fo)
+	go spreadOutIn(fo, fi)
+
+	for w := range fi {
+		fmt.Println(w)
 	}
 	fmt.Println("exiting")
 }
 
-func spreadChans(m3, nm3 chan<- int) {
-	for i := 0; i < 15; i++ {
-		if i%3 == 0 {
-			m3 <- i
-		} else {
-			nm3 <- i
-		}
+func fillChan(x chan int) {
+	for i := 0; i < 90; i++ {
+		x <- i
 	}
-	close(m3)
-	close(nm3)
+	close(x)
 }
 
-func getToPipe(m3, nm3 <-chan int, p chan<- int) {
+func spreadOutIn(fo, fi chan int) {
 	var wg sync.WaitGroup
-	wg.Add(2)
+	const numGos = 15
+	wg.Add(numGos)
 
-	go func() {
-		for v := range m3 {
-			p <- v
-		}
-		wg.Done()
-	}()
-
-	go func() {
-		for y := range nm3 {
-			p <- y
-		}
-		wg.Done()
-	}()
-
+	for i := 0; i < numGos; i++ {
+		go func() {
+			for j := range fo {
+				func(y int) {
+					fi <- heavyComputing(y)
+				}(j)
+			}
+			wg.Done()
+			close(fi)
+		}()
+	}
 	wg.Wait()
-	close(p)
+	close(fo)
+}
+
+func heavyComputing(z int) int {
+	time.Sleep(time.Microsecond * time.Duration(rand.Intn(5)))
+	return z + rand.Intn(10)
 }
